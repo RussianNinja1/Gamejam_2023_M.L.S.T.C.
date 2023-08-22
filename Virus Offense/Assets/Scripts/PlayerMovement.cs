375980f1359e33a8ca0b100d8b93f065ab6ce4a0
TraveenW <traveenw@gmail.com>
2023-08-22 23:15:29 +1000

    Updated player script for camera following capabilities

--- Virus Offense/Assets/Scripts/PlayerMovement.cs
+++ Virus Offense/Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,13 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3;
 
+    [Header("Camera Attachment")]
+    [SerializeField] GameObject attachedCamera;
+    [SerializeField] bool isAttached = false;
+
     float moveHorizontal;
     float moveVertical;
+    
 
     // Translate this object by the horizontal and vertical input amounts, multiplied by move speed
     private void FixedUpdate()
@@ -17,5 +22,11 @@ public class PlayerMovement : MonoBehaviour
         moveVertical = Input.GetAxisRaw("Vertical");
 
         transform.Translate(new Vector2(moveHorizontal, moveVertical) * moveSpeed * Time.fixedDeltaTime, Space.World);
+        
+        // When isAttached is true, let camera follow player position
+        if (isAttached)
+        {
+            attachedCamera.transform.position = new Vector3(transform.position.x, transform.position.y, attachedCamera.transform.position.z);
+        }
     }
 }
