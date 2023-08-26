3512d2589a97d94bbd95012a241e1eb0f965eaeb
TraveenW <traveenw@gmail.com>
2023-08-26 16:56:04 +1000

    Added HealthBar and PlayerHealth scripts; Updated SampleScene with those scripts

---
+++ Virus Offense/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    Slider healthSlider;
+
+    private void Start()
+    {
+        healthSlider = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        // Hides bar when its value is either 0 or the max HP value. Reveals it otherwise.
+        if (healthSlider.value == 0 || healthSlider.value == healthSlider.maxValue)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    //Sets the variables to their max value at start.
+    public void SetMaxHealth(int maxHealth)
+    {
+        healthSlider.maxValue = maxHealth;
+    }
+
+    //used to update the UI to the player's current health value
+    public void SetHealth(int health)
+    {
+        healthSlider.value = health;
+    }
+}
---
+++ Virus Offense/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int maxHealth = 100;
+    [SerializeField] float immunityTime = 0.1f;
+    [SerializeField] bool destroyEnemyOnContact = true;
+    [SerializeField] int currentHealth;
+
+    bool isImmune = false;
+    HealthBar healthBar;
+
+    void Start()
+    {
+        healthBar = GetComponentInChildren<HealthBar>();
+
+        // Set the our health and reset the health bar
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        healthBar.SetHealth(maxHealth);
+    }
+
+    // Destroy other object if it's an enemy, when player isn't immune, and when destroyenemyOnContact is set to true
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && isImmune == false && destroyEnemyOnContact == true)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Update health and the HP bar.
+    // If it is damage, it can be affected by immunity time
+    public void UpdateHealth(int changeOfHealth, bool isDamage = true)
+    {
+        if (isDamage == true && isImmune == false) // Subtract by changeOfHealth if damage recieved when not immune. Triggers immunity.
+        {
+            currentHealth -= changeOfHealth;
+            healthBar.SetHealth(currentHealth);
+            StartCoroutine(Immunity());
+        }
+        else if (isDamage == false) // Add by changeOfHealth if it isn't damage being recieved
+        {
+            currentHealth += changeOfHealth;
+            healthBar.SetHealth(currentHealth);
+        }
+    }
+
+    // Prevents player from being hit too many times
+    IEnumerator Immunity()
+    {
+        isImmune = true;
+        yield return new WaitForSeconds(immunityTime);
+        isImmune = false;
+    }
+}
